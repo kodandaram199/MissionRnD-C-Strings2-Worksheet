6db6d3f2af89aadacedd637e6873df8dcc2f3b17
kodandaram199 <kodandaram199@missionrnd.com>
2016-03-16 19:18:34 +0530

    Submit Commit

--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -1,11 +1,22 @@
 #include <stdlib.h>
-int length(char *str)
+int strlen(char *str)
 {
 	int i = 0;
 	while (str[i++] != '\0');
 	return i;
 }
 char * get_last_word(char * str){
-	return NULL;
+	char * result = NULL;
+	int length = strlen(str);
+	int end = length - 1;
+	int temp = end;
+	while (str[temp - 1] != ' ' && temp>0)
+		temp--;
+	result = (char *)malloc(sizeof(char)*(end - temp + 1));
+	for (int i = temp; i <= end; i++){
+		result[i - temp] = str[i];
+	}
+	result[end - temp + 1] = '\0';
+	return result;
 }
 
